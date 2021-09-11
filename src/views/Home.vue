1f38b99ed4575b771f7c52bf369eb11153c21ce0
jiayenli <x9276550@gmail.com>
2021-09-11 14:48:21 +0800

    sweet alert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about home</h1>
+    <h1 @click="alertTest"> This is an Home home</h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
+h1 {
+  cursor: pointer;
+}
 
 </style>
+
+<script>
+import Swal from 'sweetalert2'
+export default {
+  
+  methods: {
+    alertTest() {
+      Swal.fire('有成功跳出通知嗎')
+    }
+
+  },
+}
+</script>
